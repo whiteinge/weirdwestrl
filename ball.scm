9e1f6932ebefe18603d5d927c4d4ffe19ec975b6
Seth House <seth@eseth.com>
2020-01-14 00:39:47 -0700

    Recursively draw an empty dungeon floor

--- ball.scm
+++ ball.scm
@@ -2,37 +2,61 @@
     chicken.format
     chicken.port
     chicken.process.signal
+    chicken.random
     srfi-12
     srfi-18
     (prefix ncurses ncurses:))
 
 (define log-port (open-output-file "out.log"))
 (set-buffering-mode! log-port #:line)
+(define (out s . args)
+    (apply format log-port (string-append s "\n") args))
 
-(define y 0)
-(define x 0)
-(define max_y 0)
-(define max_x 0)
-(define next_x 0)
-(define direction 1)
+(define (draw-floor y x max_y max_x)
+    (when (and (< y max_y) (< x max_x))
+        (out "~a ~a" y x)
+        (ncurses:mvprintw y x ".")
+        (draw-floor (+ y 1) (+ x 1) max_y max_x)))
+
+(define (draw-floor y x max_y max_x)
+    (out "~a ~a ~a ~a" y x max_y max_x)
+    (cond
+        ((= y max_y) '())
+        ((= x max_x) (draw-floor (+ y 1) 0 max_y max_x))
+        ((< x max_x) (begin
+            (ncurses:mvprintw y x ".")
+            (draw-floor y (+ x 1) max_y max_x)))))
 
 (define (draw)
+    (define y 0)
+    (define x 0)
+    (define max_y 0)
+    (define max_x 0)
+
+    (define scrnum (ncurses:stdscr))
+    (set!-values (max_y max_x) (ncurses:getmaxyx scrnum))
+
+    (ncurses:clear)
+    ; Why do I get an out of bounds error when writing to the last column?
+    ; Maybe it writes then moves the (hidden) cursor to the next column which
+    ; is then out of bounds? Do I still get out of bounds errors when writing
+    ; to non-root windows?
+    (draw-floor 0 0 max_y (- max_x 1))
+    (ncurses:refresh)
+
+    (out "Done with the drawing")
+
     (let loop ()
-        (define scrnum (ncurses:stdscr))
-        (set!-values (max_y max_x) (ncurses:getmaxyx scrnum))
-
-        (ncurses:clear)
-        (ncurses:mvprintw y x "@")
-        (ncurses:refresh)
-
-        (thread-sleep! 0.1)
+        ; (ncurses:clear)
+        ; (ncurses:mvprintw y x "@")
+        ; (ncurses:refresh)
 
         (case (ncurses:getch)
             ((#\k) (set! y (- y 1)))
             ((#\j) (set! y (+ y 1)))
             ((#\l) (set! x (+ x 1)))
             ((#\h) (set! x (- x 1)))
-            (else (display "DOH\n" log-port)))
+            (else (out "DOH")))
 
         (loop)))
 
